e5de50b4ed3dcaa418aeb587be7ac04aeb4d982a
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-09-17 17:46:30 +0100

    modified DbContext

--- WebApplication1/Data/AppDbContext.cs
+++ WebApplication1/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Configuration;
 using WebApplication1.Models;
